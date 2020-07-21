92d4fca6a3f432b313b030495e588e151257319d
JhanCBB117 <bastidasmartinezj@gmail.com>
2020-07-21 13:55:09 -0500

    Formulario de Nueva Planeacion Arreglado

--- app/controllers/agentes/planeaciones_controller.rb
+++ app/controllers/agentes/planeaciones_controller.rb
@@ -59,6 +59,6 @@ class Agentes::PlaneacionesController < ApplicationController
   end
 
   def planeacion_params
- 		params.require(:planeacion).permit(:formato, :nombre, :tematica, :estado) 	
+ 		params.require(:planeacion).permit(:formato, :tematica, :estado) 	
   end
 end
