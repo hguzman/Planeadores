f0b3cd964ef546d51376b851f6045e355c601e67
JhanCBB117 <bastidasmartinezj@gmail.com>
2020-07-27 18:51:18 -0500

    Validacion de los formulario

--- app/models/agente.rb
+++ app/models/agente.rb
@@ -2,5 +2,10 @@ class Agente < ApplicationRecord
 	has_many :planeaciones
 	has_many :transacciones
 
+	validates :primerNombre, :primerApellido, :nombreUds, presence: true
+	validates_format_of :correo,  :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 	validates :cedula, uniqueness: true
+	validates :cedula, numericality: true
+	validates :cedula, length: {maximum: 10}
+  validates :cedula, length: {minimum: 10}
 end
--- db/migrate/20200701020644_add_agente_to_ventas.rb
+++
@@ -1,5 +0,0 @@
-class AddAgenteToVentas < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :ventas, :agente, null: false, foreign_key: true
-  end
-end
