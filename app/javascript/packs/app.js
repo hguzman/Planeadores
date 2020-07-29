8357dca00a502e36855cbb71e85478bc3f87ba72
Cristian Julio <cristianandres2001julio@gmail.com>
2020-07-29 17:18:29 -0500

    Ahora el mensaje flash se muestra rojo si es un error

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -7,17 +7,16 @@ const menu = document.getElementById('btnSubmenu');
 // EventListeners
 if (message.innerHTML !== '') {
 	document.addEventListener('DOMContentLoaded', colocarMessage(message));
-	console.log(message.textContent);
 }
 
 if (messageAlert.innerHTML !== '') {
-	colocarMessage(messageAlert);
+	colocarMessage(messageAlert, 'success');
 }
 
 try {
 	if (messageError.innerHTML !== '') {
-		colocarMessage(messageError);
-		messageError.innerHTML = `La cédula ingresada ya está registrada o los campos están mal`;
+		colocarMessage(messageError, 'error');
+		// messageError.innerHTML = `La cédula ingresada ya está registrada o los campos están mal`;
 	}
 } catch(error) {
 	console.log(`Error: ${error}`);
@@ -26,9 +25,15 @@ try {
 menu.addEventListener('click', mostrarMenu);
 
 // Funciones
-function colocarMessage(elemento) {
+function colocarMessage(elemento, tipo) {
 	elemento.style.display = 'block';
 	elemento.style.animation = 'flashMessage 1s';
+
+	if(tipo === 'error') {
+		elemento.style.background = '#FFC3C3';
+		elemento.style.color = '#FF3333';
+		elemento.style.border = '2px solid #FF3333';
+	}
 
 	setTimeout(function() {
 		elemento.style.animation = 'quitarMessage 1s';
