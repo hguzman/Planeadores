7159bbde0cc72f103b4e26eb6d5917e6af2959a5
Cristian Julio <cristianandres2001julio@gmail.com>
2020-08-11 15:48:27 -0500

    Cambios

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = Usuario.create(email: "admin@gmail.com", password: 123456, password_confirmation: 123456, primerNombre: "Cristian", segundoNombre: "Andres", primerApellido: "Julio")
+usuario = Usuario.create(email: "usuario@gmail.com", password: 123456, password_confirmation: 123456, primerNombre: "Andrea", segundoNombre: "Camila", primerApellido: "Davila")
